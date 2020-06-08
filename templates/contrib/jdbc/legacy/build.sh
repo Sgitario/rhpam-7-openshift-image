0be5030b07c4f1b676e615e47ce52206826a4dcc
Jose <josecarvajalhilario@gmail.com>
2020-06-08 08:38:05 +0200

    Example of changes

--- templates/contrib/jdbc/legacy/build.sh
+++ templates/contrib/jdbc/legacy/build.sh
@@ -11,10 +11,11 @@ function print_help() {
     echo "Builds and pushes JDBC Driver images to a docker registry. This has to be executed from a folder containing a Dockerfile"
     echo ""
     echo "Usage: "
-    echo "   ../build.sh [--registry=myregistry.example.com:5000] [--artifact-repo=https://myrepo.example.com/maven/public]"
+    echo "   ../build.sh [--registry=myregistry.example.com:5000] [--namespace=mynamespace] [--artifact-repo=https://myrepo.example.com/maven/public]"
     echo "Options:"
     echo "   Available driver images to build: db2, mariadb, mssql, mysql, oracle, postgresql, sybase"
     echo "   --registry         Specifies the docker registry to use for tagging and pushing. Defaults to docker-registry.default.svc:5000"
+    echo "   --namespace        Specifies the docker namespace to use for tagging and pushing. Defaults to openshift"
     echo "   --artifact-repo    Specifies the Maven repository where the jdbc drivers are available. Oracle does not have a default value"
     echo "   --image-tag        Specifies the tag to use when building the image. Defaults to 1.0"
 }
@@ -24,6 +25,9 @@ do
     case $1 in
         --registry=*)
             registry=${1#*=}
+        ;;
+        --namespace=*)
+            namespace=${1#*=}
         ;;
         --artifact-repo=*)
             artifact_repo=${1#*=}
@@ -54,15 +58,7 @@ current_dir=${PWD##*/}
 driver=$(echo $current_dir | cut -d '-' -f 1)
 image_tag=${image_tag:-1.0}
 
-registry=${registry:-docker-registry.default.svc:5000}
-
-function docker_login() {
-    if [[ $(oc whoami -t > /dev/null; echo $?) == 1 ]]; then
-        echo "You must be logged in"
-        exit 1
-    fi
-    docker login -u `oc whoami` -p `oc whoami -t` $registry
-}
+registry=${registry:-docker-registry.default.svc:5000}/${namespace:-openshift}
 
 function build() {
     local driver=$1
@@ -88,22 +84,22 @@ function push() {
 function create_build() {
     local driver=$1
     local version=$2
-    oc new-build -n openshift \
-        --name rhpam78-kieserver-$driver-openshift \
+    oc new-build \
+        --name rhpam-kieserver-$driver-openshift \
         --image-stream=openshift/rhpam73-kieserver-openshift:$image_tag \
-        --source-image=openshift/$driver-driver-image:$version \
+        --source-image=$registry/$driver-driver-image:$version \
         --source-image-path=/extensions:$driver-driver/ \
-        --to=rhpam78-kieserver-$driver-openshift:$image_tag \
+        --to=rhpam-kieserver-$driver-openshift:$image_tag \
         -e CUSTOM_INSTALL_DIRECTORIES=$driver-driver/extensions
 }
 
-docker_login
+# docker_login
 
 pushd ..
 
 image_name=$driver-driver-image
 version=$(grep version $current_dir/Dockerfile | awk -F"=" '{print $2}' | sed 's/"//g')
-tag=$registry/openshift/$image_name:$version
+tag=$registry/$image_name:$version
 build $image_name $tag ${artifact_repo:-}
 push $tag
 create_build $driver $version
